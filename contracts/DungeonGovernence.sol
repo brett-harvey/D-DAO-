d19cf449a7f7c91b8b8dd7db1f604ef92e568d0f
brett-harvey <brettharvey2014@gmail.com>
2017-10-13 23:11:01 -0300

    Added new files

--- contracts/DungeonGovernence.sol
+++ contracts/DungeonGovernence.sol
@@ -8,7 +8,56 @@ contract DungeonMaster {
   }
 
   modifier OnlyDungeonMaster() {
-    if (msg.sender != DM) { throw; }
+    require(msg.sender == DM);
     _;
+  }
+}
+
+contract Dungeon is DungeonMaster {
+  mapping (address => uint256) public GoldCharacterBalance;
+  mapping (address => uint256) public ManaCharacterBalance;
+  mapping (address => uint256) public FoodCharacterBalance;
+  mapping (address => uint256) public SwordInvetory;
+
+  uint public SwordPrice;
+
+  event ItemAdded(address CharacterAddress, string ItemName, uint Amount);
+
+
+  function Dungeon(address DungonMaster)  {
+    if (DungonMaster == 0) DungonMaster = msg.sender;
+    else DM = DungonMaster;
+    GoldCharacterBalance[this] = 10000;
+    ManaCharacterBalance[this] = 7500;
+    FoodCharacterBalance[this] = 5000;
+  }
+
+  function MintGold(address _character, uint _amount) OnlyDungeonMaster {
+    GoldCharacterBalance[_character] += _amount;
+    ItemAdded(_character, "Gold", _amount);
+  }
+
+  function MintMana(address _character, uint _amount) OnlyDungeonMaster {
+    ManaCharacterBalance[_character] += _amount;
+    ItemAdded(_character, "Mana", _amount);
+  }
+
+  function MintFood(address _character, uint _amount) OnlyDungeonMaster {
+    FoodCharacterBalance[_character] += _amount;
+    ItemAdded(_character, "Food", _amount);
+  }
+
+  /* Dungeon Market */
+  function SetSwordPrice(uint _price) OnlyDungeonMaster() {
+    SwordPrice = _price;
+  }
+
+  function BuySword(address _character, uint _amount) {
+    if (GoldCharacterBalance[_character] >= SwordPrice) {
+        SwordInvetory[_character] += _amount;
+        GoldCharacterBalance[_character] -= SwordPrice * _amount;
+        GoldCharacterBalance[this] += SwordPrice;
+        ItemAdded(_character, "Sword", _amount);
+      }
   }
 }
